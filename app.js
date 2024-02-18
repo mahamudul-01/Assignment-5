162a048080dec6fd27e95b45d346f708f1d73392
mahamudul-01 <mahamudul.hasan.mehedi.01@gmail.com>
2024-02-19 01:15:13 +0600

    added discount price option

--- app.js
+++ app.js
@@ -1,22 +1,52 @@
 const allSeatBtn=document.getElementsByClassName('seat-btn');
 let count=0;
 let totalSeat=40;
-const maxSeat=document.getElementById('seat-count').innerText;
-const maxSeatInt=parseInt(maxSeat);
+let totalPrice=0;
 
-if(maxSeatInt <= 4){
-    for(let seat of allSeatBtn){
-        seat.addEventListener("click",function(e){
-            count=count+1;
-            totalSeat=totalSeat-1;
-            const Id=seat.id;
-    
-            setInnerTextByID('seat-count',count);
-            setInnerTextByID('total-seat',totalSeat)
-            setBtnColor(Id);
-        })
-    }
+for(let seat of allSeatBtn){
+    seat.addEventListener("click",function(e){
+        count=count+1;
+        totalSeat=totalSeat-1;
+        totalPrice=totalPrice + 550;
+        const Id=seat.id;
+        const text=seat.innerText;
+        const container=document.getElementById('append-container')
+        const li=document.createElement("li")
+        const p=document.createElement("p")
+        const p2=document.createElement("p")
+        const p3=document.createElement("p")
+        p.innerText=text;
+        p2.innerText='Economy'
+        p3.innerText='Price: 550'
+        li.appendChild(p)
+        li.appendChild(p2)
+        li.appendChild(p3)
+        container.appendChild(li)
+        li.classList.add('flex','justify-between','mt-2','mb-2')
+        setInnerTextByID('seat-count',count);
+        setInnerTextByID('total-seat',totalSeat)
+        setInnerTextByID('total-price',totalPrice)
+        setBtnColor(Id);
+    })
 }
-else{
-    alert()
+function discountPrice(){
+    
+    const cupponCode=getInputFieldText('cuppon-btn')
+    console.log(cupponCode)
+    
+    
+    let discountFinalPrice=0;
+    if(cupponCode === 'NEW15'){
+        discountFinalPrice = totalPrice - (totalPrice * 0.15);
+        
+        setInnerTextByID('discount-price',discountFinalPrice)
+        hideElementById('cuppon-card');
+    }
+    else if(cupponCode === 'Couple 20'){
+        discountFinalPrice = totalPrice - (totalPrice * 0.20);
+        
+        setInnerTextByID('discount-price',discountFinalPrice)
+        hideElementById('cuppon-card');
+    }
+    
 }
--- utility.js
+++ utility.js
@@ -6,3 +6,17 @@ function setBtnColor(elementId){
    const element= document.getElementById(elementId).classList.add('btn-color');
 
 }
+function getInputFieldText(elementId){
+    const element=document.getElementById(elementId);
+    const elementText=element.value ;
+    return elementText;
+}
+function getText(elementId){
+    const element=document.getElementById(elementId);
+    const elementInt=parseInt(element);
+    return elementInt;
+}
+function hideElementById(elementId){
+    const element=document.getElementById(elementId);
+    element.classList.add('hidden')
+}
