3fc2dca2de4ae01b47f8b40bff2b3559d9fef785
mahamudul-01 <mahamudul.hasan.mehedi.01@gmail.com>
2024-02-19 21:49:21 +0600

    Finish

--- app.js
+++ app.js
@@ -6,42 +6,50 @@ let selectedButton=[];
 
 
 
-    
-        for(let seat of allSeatBtn){
-            seat.addEventListener("click",function(e){
-                if ( selectedButton.length < 4 ){
-                    if(! selectedButton.includes(seat.id)){
-                        count=count+1;
-                        totalSeat=totalSeat-1;
-                        totalPrice=totalPrice + 550;
-                        const Id=seat.id;
-                        selectedButton.push(Id)
-                        const text=seat.innerText;
-                        const container=document.getElementById('append-container')
-                        const li=document.createElement("li")
-                        const p=document.createElement("p")
-                        const p2=document.createElement("p")
-                        const p3=document.createElement("p")
-                        p.innerText=text;
-                        p2.innerText='Economy';
-                        p3.innerText='Price: 550'
-                        li.appendChild(p)
-                        li.appendChild(p2)
-                        li.appendChild(p3)
-                        container.appendChild(li)
-                        li.classList.add('flex','justify-between','mt-2','mb-2')
-                        setInnerTextByID('seat-count',count);
-                        setInnerTextByID('total-seat',totalSeat)
-                        setInnerTextByID('total-price',totalPrice)
-                        setBtnColor(Id);
-                        console.log(selectedButton)
-                        setBtnColor('successful-btn')
-                    }
-                
-                }
-            })
+    // Handle seat 
+for(let seat of allSeatBtn){
+    seat.addEventListener("click",function(e){
+        if ( selectedButton.length < 4 ){
+            if(! selectedButton.includes(seat.id)){
+                count=count+1;
+                totalSeat=totalSeat-1;
+                totalPrice=totalPrice + 550;
+                const Id=seat.id;
+                selectedButton.push(Id)
+                const text=seat.innerText;
+                const container=document.getElementById('append-container')
+                const li=document.createElement("li")
+                const p=document.createElement("p")
+                const p2=document.createElement("p")
+                const p3=document.createElement("p")
+                p.innerText=text;
+                p2.innerText='Economy';
+                p3.innerText='Price: 550'
+                li.appendChild(p)
+                li.appendChild(p2)
+                li.appendChild(p3)
+                container.appendChild(li)
+                li.classList.add('flex','justify-between','mt-2','mb-2')
+                setInnerTextByID('seat-count',count);
+                        
+
+                setInnerTextByID('total-seat',totalSeat)
+                setInnerTextByID('total-price',totalPrice)
+                setInnerTextByID('discount-price',totalPrice);
+                setBtnColor(Id);
+                console.log(selectedButton)
+                setBtnColor('successful-btn')
         }
-   
+        else{
+                alert('You cannot select one seat double time !')
+            }
+                
+        }
+    })
+}
+  
+
+// This function calculate the discount price and set discount price
 function discountPrice(){
     
     const cupponCode=getInputFieldText('cuppon-btn')
@@ -62,6 +70,9 @@ function discountPrice(){
         setInnerTextByID('discount-price',discountFinalPrice)
         hideElementById('cuppon-card');
         showElementById('claimed')
+    }
+    else{
+        alert('Chek your cuppon code or book any seat !')
     }
     
 }
--- script/utility.js
+++ script/utility.js
@@ -2,24 +2,29 @@
 function setInnerTextByID(elementId,textValue){
     document.getElementById(elementId).innerText=textValue;
 }
+// set button color
 function setBtnColor(elementId){
    const element= document.getElementById(elementId).classList.add('btn-color');
 
 }
+// get input input field
 function getInputFieldText(elementId){
     const element=document.getElementById(elementId);
     const elementText=element.value ;
     return elementText;
 }
+// get element text
 function getText(elementId){
     const element=document.getElementById(elementId);
     const elementInt=parseInt(element);
     return elementInt;
 }
+// hide element
 function hideElementById(elementId){
     const element=document.getElementById(elementId);
     element.classList.add('hidden')
 }
+// display element
 function showElementById(elementId){
     const element=document.getElementById(elementId);
     element.classList.remove('hidden')
