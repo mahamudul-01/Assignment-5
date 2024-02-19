9740a89c0a0a7d6842ddb3a0e3b38fc0c212cf53
mahamudul-01 <mahamudul.hasan.mehedi.01@gmail.com>
2024-02-19 21:04:00 +0600

    almost complete

--- app.js
+++ app.js
@@ -2,33 +2,46 @@ const allSeatBtn=document.getElementsByClassName('seat-btn');
 let count=0;
 let totalSeat=40;
 let totalPrice=0;
+let selectedButton=[];
 
-for(let seat of allSeatBtn){
-    seat.addEventListener("click",function(e){
-        count=count+1;
-        totalSeat=totalSeat-1;
-        totalPrice=totalPrice + 550;
-        const Id=seat.id;
-        const text=seat.innerText;
-        const container=document.getElementById('append-container')
-        const li=document.createElement("li")
-        const p=document.createElement("p")
-        const p2=document.createElement("p")
-        const p3=document.createElement("p")
-        p.innerText=text;
-        p2.innerText='Economy'
-        p3.innerText='Price: 550'
-        li.appendChild(p)
-        li.appendChild(p2)
-        li.appendChild(p3)
-        container.appendChild(li)
-        li.classList.add('flex','justify-between','mt-2','mb-2')
-        setInnerTextByID('seat-count',count);
-        setInnerTextByID('total-seat',totalSeat)
-        setInnerTextByID('total-price',totalPrice)
-        setBtnColor(Id);
-    })
-}
+
+
+    
+        for(let seat of allSeatBtn){
+            seat.addEventListener("click",function(e){
+                if ( selectedButton.length < 4 ){
+                    if(! selectedButton.includes(seat.id)){
+                        count=count+1;
+                        totalSeat=totalSeat-1;
+                        totalPrice=totalPrice + 550;
+                        const Id=seat.id;
+                        selectedButton.push(Id)
+                        const text=seat.innerText;
+                        const container=document.getElementById('append-container')
+                        const li=document.createElement("li")
+                        const p=document.createElement("p")
+                        const p2=document.createElement("p")
+                        const p3=document.createElement("p")
+                        p.innerText=text;
+                        p2.innerText='Economy';
+                        p3.innerText='Price: 550'
+                        li.appendChild(p)
+                        li.appendChild(p2)
+                        li.appendChild(p3)
+                        container.appendChild(li)
+                        li.classList.add('flex','justify-between','mt-2','mb-2')
+                        setInnerTextByID('seat-count',count);
+                        setInnerTextByID('total-seat',totalSeat)
+                        setInnerTextByID('total-price',totalPrice)
+                        setBtnColor(Id);
+                        console.log(selectedButton)
+                        setBtnColor('successful-btn')
+                    }
+                
+                }
+            })
+        }
+   
 function discountPrice(){
     
     const cupponCode=getInputFieldText('cuppon-btn')
@@ -36,17 +49,21 @@ function discountPrice(){
     
     
     let discountFinalPrice=0;
-    if(cupponCode === 'NEW15'){
+    if(cupponCode === 'NEW15' && selectedButton.length !==0){
         discountFinalPrice = totalPrice - (totalPrice * 0.15);
         
         setInnerTextByID('discount-price',discountFinalPrice)
         hideElementById('cuppon-card');
+        showElementById('claimed')
     }
-    else if(cupponCode === 'Couple 20'){
+    else if(cupponCode === 'Couple 20'  && selectedButton.length !==0){
         discountFinalPrice = totalPrice - (totalPrice * 0.20);
         
         setInnerTextByID('discount-price',discountFinalPrice)
         hideElementById('cuppon-card');
+        showElementById('claimed')
     }
     
 }
+
+
--- utility.js
+++ utility.js
@@ -20,3 +20,7 @@ function hideElementById(elementId){
     const element=document.getElementById(elementId);
     element.classList.add('hidden')
 }
+function showElementById(elementId){
+    const element=document.getElementById(elementId);
+    element.classList.remove('hidden')
+}
